a73b07d5444ee388551e3d49eb607b2b30879041
Ryan <sullivan050ryan@gmail.com>
2023-02-24 15:39:02 -0800

    just in case

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,17 +1,40 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { UserContext } from '../context/UserContext'
 
 const Login = () => {
+    const {investor} = useContext(UserContext)
+    const loginFormData = {
+        email:'',
+        password: ''
+    }
+
+
+    const loginSubmit = e => {
+        e.preventDefault()
+        // console.log('submit')
+        fetch('/signin', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(investor)
+        })
+    }
+
   return (
     <div className='loginFormParent'>
-      <form 
-        className='loginForm'>
-        <input 
-            placeholder='Enter Email'
-        ></input>
-        <input 
-            
-            placeholder='Enter Password'
-        ></input>
+        <form 
+            onSubmit={loginSubmit}
+            className='loginForm'
+        >
+            <input 
+                placeholder='Enter Email'
+            />
+            <input 
+                name='password'
+                placeholder='Enter Password'
+            />
+            <button>Sign In</button> <br/>
+            <Link to='/signup'>Create an account</Link>
 
       </form>
     </div>
