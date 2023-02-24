6168005a7e5a9d8b0ed976d2b31aaaa4f6dd5d05
Ryan <sullivan050ryan@gmail.com>
2023-02-24 10:52:47 -0800

    add crud actions to controllers

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,15 +1,19 @@
 class StocksController < ApplicationController
-
+    before_action :find_stock, only: [:show]
 
 
     def index 
         render json: Stock.all
     end
 
+    def show
+        render json: @stock
+    end
+
     private
 
     def find_stock
-        
+        @stock = Stock.find(params[:id])
     end
 
 
--- db/migrate/20230223174137_create_portfolios.rb
+++ db/migrate/20230223174137_create_portfolios.rb
@@ -2,7 +2,7 @@ class CreatePortfolios < ActiveRecord::Migration[7.0]
   def change
     create_table :portfolios do |t|
       t.integer :quantity
-
+      
       t.timestamps
     end
   end
