6be3ea7d696bcbf0471040c902148e30ee425dcb
Ryan <sullivan050ryan@gmail.com>
2023-03-01 17:50:11 -0800

    MUI signup

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,7 +1,20 @@
 import React, { useContext, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { UserContext } from '../context/UserContext'
 // import { TextField } from '@mui/material'
+
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 
 const Signup = ({setToggleAuth}) => {
@@ -22,7 +35,7 @@ const Signup = ({setToggleAuth}) => {
     setSignupFormData(currentUser => (
       {...currentUser, [name]: value}
     ))
-    console.log('typing in signup')
+    console.log(e.target.value)
   }
 
   const signupSubmit = e => {
@@ -45,49 +58,161 @@ const Signup = ({setToggleAuth}) => {
     })
   }
 
+  function Copyright(props) {
+    return (
+      <Typography variant="body2" color="text.secondary" align="center" {...props}>
+        {'Copyright © '}
+        <Link color="inherit" href="https://mui.com/">
+          Your Website
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    );
+  }
+  
+  const theme = createTheme();
+
   return (
-    <div className='signupFormParent'>
-      <form 
-        onSubmit={e => signupSubmit(e, signupFormData)}
-        className='signupForm'>
-        <input 
-            className='signupInput'
-            placeholder='Enter First Name'
-            onChange={signupHandleChange}
-            name='first_name'
-            value={signupFormData.first_name}
-            ></input>
 
-        <input 
-            className='signupInput'
-            onChange={signupHandleChange}
-            placeholder='Enter Last Name'
-            name='last_name'
-            value={signupFormData.last_name}
-            ></input>
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" noValidate onSubmit={e => signupSubmit(e, signupFormData)} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  onChange={signupHandleChange}                  
+                  name="first_name"
+                  value={signupFormData.first_name}
+                  required
+                  fullWidth
+                  id="first_name"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  onChange={signupHandleChange}
+                  required
+                  fullWidth
+                  id="lastName"
+                  value={signupFormData.last_name}
+                  label="Last Name"
+                  name="last_name"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  onChange={signupHandleChange}                  
+                  id="email"
+                  label="Email Address"
+                  value={signupFormData.email}
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  onChange={signupHandleChange}                  
+                  name="password"
+                  value = {signupFormData.password}
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="login" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </ThemeProvider>
 
-        <input 
-            className='signupInput'
-            onChange={signupHandleChange}
-            placeholder= 'Enter Email'
-            name='email'
-            value={signupFormData.email}
-            ></input>
+    // <div className='signupFormParent'>
+    //   <form 
+    //     onSubmit={e => signupSubmit(e, signupFormData)}
+    //     className='signupForm'>
+    //     <input 
+    //         className='signupInput'
+    //         placeholder='Enter First Name'
+    //         onChange={signupHandleChange}
+    //         name='first_name'
+    //         value={signupFormData.first_name}
+    //         ></input>
 
-        <input 
-            className='signupInput'
-            onChange={signupHandleChange}
-            placeholder='Enter New Password'
-            name='password'
-            value={signupFormData.password}
-            ></input>
-        <button>Sign Up</button> <br/>
+    //     <input 
+    //         className='signupInput'
+    //         onChange={signupHandleChange}
+    //         placeholder='Enter Last Name'
+    //         name='last_name'
+    //         value={signupFormData.last_name}
+    //         ></input>
+
+    //     <input 
+    //         className='signupInput'
+    //         onChange={signupHandleChange}
+    //         placeholder= 'Enter Email'
+    //         name='email'
+    //         value={signupFormData.email}
+    //         ></input>
+
+    //     <input 
+    //         className='signupInput'
+    //         onChange={signupHandleChange}
+    //         placeholder='Enter New Password'
+    //         name='password'
+    //         value={signupFormData.password}
+    //         ></input>
+    //     <button>Sign Up</button> <br/>
      
-      </form>
-      <Link to='/login'>Have an Account? Sign in here. 
-      </Link>
+    //   </form>
+    //   <Link to='/login'>Have an Account? Sign in here. 
+    //   </Link>
       
-    </div>
+    // </div>
   )
 }
 
